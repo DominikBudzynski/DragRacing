e46031b99a8f4385a6251dc8b5290005590a7e50
DominikBudzynski <dominik.budzynski9@gmail.com>
2023-06-21 23:09:16 +0200

    Added RaceCar and OffRoadCar

---
+++ DragRacing/OffRoadCar.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragRacing
+{
+    internal class OffRoadCar : Car
+    {
+        private double height;
+        private double suspension;
+
+        public OffRoadCar() : base()
+        {
+            height = 0;
+            suspension = 0;
+        }
+
+        public OffRoadCar(double enginePower, double grip, int gearBox, double height, double suspension, string model) : 
+                     base(enginePower, grip, gearBox, model)
+        {
+            Height = height;
+            Suspension = suspension;
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (value > 0) height = value;
+            }
+        }
+
+        public double Suspension
+        {
+            get { return  suspension; }
+            set
+            {
+                if (value > 0) suspension = value;
+            }
+        }
+
+        public override double Accelerate(double distance)
+        {
+            return 0;
+        }
+    }
+}
---
+++ DragRacing/RaceCar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragRacing
+{
+    class RaceCar : Car
+    {
+        private double turbo;
+        private double aerodynamics;
+        public RaceCar() : base()
+        {
+            turbo = 1;
+            aerodynamics = 1;
+        }
+
+        public RaceCar(double enginePower, double grip, int gearBox, double turbo, double aerodynamics, string model) : 
+                  base(enginePower, grip, gearBox, model)
+        {
+            Turbo = turbo;
+            Aerodynamics = aerodynamics;
+        }
+
+        public double Turbo {
+            get { return turbo; }
+            set
+            {
+                if (value > 0)
+                {
+                    turbo = value;
+                }
+            }
+        }
+
+        public double Aerodynamics
+        {
+            get { return aerodynamics; }
+            set
+            {
+                if (value > 0) aerodynamics = value;
+            }
+        }
+
+        public override double Accelerate(double distance)
+        {
+            return 0;
+        }
+
+    }
+}
